00993628ce284956c47dd59f8aad606ed7516b52
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-08 21:24:01 +0200

    + header padding

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	export let paddingY = 'py-32';
+	export let paddingX = 'px-16 xl:px-2';
 </script>
 
-<header class="w-full min-h-screen flex justify-center items-center">
+<header class="w-full min-h-screen flex justify-center items-center {paddingY} {paddingX}">
 	<div class="xl:w-site mx-auto flex flex-col xl:flex-row justify-center items-center gap-20">
 		<slot />
 	</div>
