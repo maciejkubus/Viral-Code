181cd89f15612cde1d7049fb668fdb9cfc77a118
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-08 23:56:29 +0200

    + footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="w-full py-32 px-16 xl:px-2 variant-filled-secondary">
+	<div class="xl:w-site mx-auto flex flex-col xl:flex-row justify-center gap-20">
+		<div class="w-full xl:w-1/4">
+			<slot name="col-1" />
+		</div>
+		<div class="w-full xl:w-1/4">
+			<slot name="col-2" />
+		</div>
+		<div class="w-full xl:w-1/4">
+			<slot name="col-3" />
+		</div>
+		<div class="w-full xl:w-1/4">
+			<slot name="col-4" />
+		</div>
+	</div>
+	<div class="xl:w-site mx-auto mt-8 pt-8 border-t-2 border-white">
+		<slot name="bottom" />
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,11 +6,70 @@
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
+	import Footer from '$lib/components/Footer.svelte';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
-<AppShell>
+<AppShell slotFooter="w-full">
 	<!-- <svelte:fragment slot="header">Header</svelte:fragment> -->
 	<slot />
-	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+	<svelte:fragment slot="footer">
+		<Footer>
+			<div slot="col-1" class="space-y-4">
+				<h2 class="font-heading-token text-4xl font-black">Viral Code</h2>
+				<p class="text-xl">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, minus?
+				</p>
+				<div class="flex gap-4 text-xl">
+					<a
+						href="https://www.facebook.com/maciek.kubus.7/"
+						target="_blank"
+						class="hover:scale-110 hover:-rotate-2"
+					>
+						<i class="fab fa-facebook-square" />
+					</a>
+					<a
+						href="https://www.instagram.com/maciekkubus/"
+						target="_blank"
+						class="hover:scale-110 hover:-rotate-2"
+					>
+						<i class="fab fa-instagram" />
+					</a>
+					<a
+						href="https://github.com/maciejkubus/"
+						target="_blank"
+						class="hover:scale-110 hover:-rotate-2"
+					>
+						<i class="fab fa-github" />
+					</a>
+				</div>
+			</div>
+			<div slot="col-2" class="text-xl flex flex-col gap-2">
+				<h3 class="text-3xl font-heading-token font-bold pb-2">Location</h3>
+				<p>
+					Mokra 21,<br />
+					95-020 Janówka
+				</p>
+				<p>
+					Lniana 2 m. 21,<br />
+					91-158 Łódź
+				</p>
+			</div>
+			<div slot="col-3" class="text-xl flex flex-col gap-2">
+				<h3 class="text-3xl font-heading-token font-bold pb-2">Links</h3>
+				<a href="/contact">Kontakt</a>
+				<a href="/offer">Oferta</a>
+				<a href="/get-to-know-us">Get to know us</a>
+				<a href="/privacy-policy">Privacy Policy</a>
+			</div>
+			<div slot="col-4" class="text-xl flex flex-col gap-2">
+				<h3 class="text-3xl font-heading-token font-bold pb-2">Posts</h3>
+				<a href="/">How to choose colors?</a>
+				<a href="/">Why you should use svelte?</a>
+				<a href="/">Abstract precisions is needed.</a>
+			</div>
+
+			<div slot="bottom">Copyright (c) Maciej Kubus 2023</div>
+		</Footer>
+	</svelte:fragment>
 </AppShell>
