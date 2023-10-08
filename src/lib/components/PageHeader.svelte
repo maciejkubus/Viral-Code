9486537bcba8179a96b3a97bd7451d94fb03dabb
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-09 00:29:37 +0200

    fix pageheader height

--- src/lib/components/PageHeader.svelte
+++ src/lib/components/PageHeader.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	export let paddingY = 'py-32';
 	export let paddingX = 'px-16 xl:px-2';
+  export let height = 'min-h-screen';
 </script>
 
-<header class="w-full min-h-screen flex justify-center items-center {paddingY} {paddingX}">
+<header class="w-full {height} flex justify-center items-center {paddingY} {paddingX}">
 	<div class="xl:w-site mx-auto flex flex-col xl:flex-row justify-center items-center gap-20">
 		<slot />
 	</div>
