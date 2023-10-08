14568281b58ab38f830c0b5bd1746b0b9cd843fb
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-08 20:46:35 +0200

    + buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,17 @@
 
 <Page>
 	<Header>
-		<div class="flex flex-col gap-8 w-2/3">
+		<div class="flex flex-col gap-12 w-2/3">
 			<h1 class="font-heading-token text-9xl font-extrablack text-secondary-500 -rotate-1">
 				Viral Code
 			</h1>
 			<p class="font-heading-token text-8xl font-black text-tertiary-500">
 				Best website building team.
 			</p>
+			<div class="flex gap-10">
+				<a href="/about-us" class="btn variant-filled-primary btn-xl">Get to know us</a>
+				<a href="/contact" class="btn variant-filled-secondary btn-xl">Contact us</a>
+			</div>
 		</div>
 		<div class="w-1/3">
 			<ImageCards images={['/pics/logo.png', '/pics/logo2.png']} href="#" />
