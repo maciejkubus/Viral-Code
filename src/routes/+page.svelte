f91198ac42b5717e271d6b50f152b11954406da3
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-08 20:48:29 +0200

    + rwd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,18 +7,20 @@
 <Page>
 	<Header>
 		<div class="flex flex-col gap-12 w-2/3">
-			<h1 class="font-heading-token text-9xl font-extrablack text-secondary-500 -rotate-1">
+			<h1
+				class="font-heading-token text-8xl xl:text-9xl font-extrablack text-secondary-500 -rotate-1"
+			>
 				Viral Code
 			</h1>
-			<p class="font-heading-token text-8xl font-black text-tertiary-500">
+			<p class="font-heading-token text-6xl xl:text-8xl font-black text-tertiary-500">
 				Best website building team.
 			</p>
-			<div class="flex gap-10">
-				<a href="/about-us" class="btn variant-filled-primary btn-xl">Get to know us</a>
-				<a href="/contact" class="btn variant-filled-secondary btn-xl">Contact us</a>
+			<div class="flex gap-4 xl:gap-10 flex-col xl:flex-row">
+				<a href="/about-us" class="btn variant-filled-primary btn-lg xl:btn-xl">Get to know us</a>
+				<a href="/contact" class="btn variant-filled-secondary btn-lg xl:btn-xl">Contact us</a>
 			</div>
 		</div>
-		<div class="w-1/3">
+		<div class="w-1/3 hidden xl:flex">
 			<ImageCards images={['/pics/logo.png', '/pics/logo2.png']} href="#" />
 		</div>
 	</Header>
