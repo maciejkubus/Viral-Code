3f392f270dbb41e5b514c3b8e6c8fafc2769ec31
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-09 04:51:56 +0200

    + portfolio page

--- src/routes/offer/+page.svelte
+++ src/routes/offer/+page.svelte
@@ -19,10 +19,6 @@
 			<p class="font-heading-token text-6xl xl:text-8xl font-black text-tertiary-500">
 				Take a look on what we offer
 			</p>
-			<div class="flex gap-4 xl:gap-10 flex-col xl:flex-row">
-				<a href="/about-us" class="btn variant-filled-primary btn-lg xl:btn-xl">Get to know us</a>
-				<a href="/contact" class="btn variant-filled-secondary btn-lg xl:btn-xl">Contact us</a>
-			</div>
 		</div>
 		<div class="w-1/3 hidden xl:flex">
 			<ImageCards images={['/pics/logo.png', '/pics/logo2.png']} href="#" />
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import PageHeader from '$lib/components/PageHeader.svelte';
+	import ImageCards from '$lib/components/ImageCards.svelte';
+	import Page from '$lib/components/Page.svelte';
+</script>
+
+<Page>
+	<PageHeader>
+		<div class="flex flex-col gap-12 xl:w-2/3">
+			<h1
+				class="font-heading-token text-8xl xl:text-9xl font-extrablack text-secondary-500 -rotate-1"
+			>
+				Viral Code
+			</h1>
+			<p class="font-heading-token text-6xl xl:text-8xl font-black text-tertiary-500">
+				Here are our achievments
+			</p>
+		</div>
+		<div class="w-1/3 hidden xl:flex">
+			<ImageCards images={['/pics/logo.png', '/pics/logo2.png']} href="#" />
+		</div>
+	</PageHeader>
+</Page>
