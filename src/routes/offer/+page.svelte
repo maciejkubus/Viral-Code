a118639ec9e95907c8a7745535f927a723898c23
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-10-09 04:28:32 +0200

    + partners

--- src/routes/offer/+page.svelte
+++ src/routes/offer/+page.svelte
@@ -78,4 +78,27 @@
 			</IconText>
 		</div>
 	</Container>
+	<Container
+		bg="variant-filled-tertiary"
+		paddingX="px-0"
+		size="w-full"
+		titlePadding="pb-32"
+		paddingY="py-0"
+	>
+		<div class="w-full flex flex-row flex-wrap">
+			{#each [1, 2, 3, 4] as num}
+				<a
+					href="/pics/amy/amy-{num}.jpg"
+					target="_blank"
+					class="w-1/2 xl:w-1/4 aspect-square flex overflow-hidden group"
+				>
+					<img
+						src="/pics/amy/amy-{num}.jpg"
+						alt="partner {num}"
+						class="w-full h-full object-cover group-hover:scale-110 group-hover:-rotate-6 transition-all duration-200 ease-in-out"
+					/>
+				</a>
+			{/each}
+		</div>
+	</Container>
 </Page>
